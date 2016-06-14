4591360d79ba0d6e7ad5464a8e1f8bb51e24d5ef
Richard Chao <r.chao03@gmail.com>
2016-06-14 15:04:01 -0400

    swig working

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-var express = require ("express");
+var Express = require ("express");
 var swig = require("swig");
 var app = new Express ();
 
-// app.get("/", function(req, res, next){
-//   res.send("welcome ")
-// next();
-// })
 
-app.engine("html", swig.renderFile)
-app.set('view engine', 'html');
-app.set('views', '/views/index.html');
+
+ // app.engine("html", swig.renderFile)
+ // app.set('view engine', 'html');
+ // app.set('views', '/views/index.html');
 
 var locals = {
     title: 'An Example',
@@ -20,20 +17,33 @@ var locals = {
     ]
 };
 
-swig.renderFile('./views/index.html', locals);
 
 
-app.use(function(req, res, next){
-  console.log(req.method, req.path)
-  console.log(res.statusCode)
-  next();
+
+// app.use(function(req, res, next){
+//   console.log(req.method, req.path)
+//   console.log(res.statusCode)
+//   next();
+// })
+
+app.get("", function(req, res, next){
+  var output =  swig.renderFile('./views/index.html', locals, function (err, output){
+ if(err) next(err)
+      else {
+          res.set('Content-Type', 'text/html');
+          res.send(output)
+      }
+      next()
+});
+
+// next();
 })
 
 
-app.use("/special/", function(req, res, next){
-  res.send("you reached the special area ")
-next();
-})
+// app.use("/special/", function(req, res, next){
+//   res.send("you reached the special area ")
+// next();
+// })
 
 
 
