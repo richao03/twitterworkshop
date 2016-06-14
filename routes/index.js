08f2da27572a64602874635261944cff3c161769
Richard Chao <r.chao03@gmail.com>
2016-06-14 17:33:35 -0400

    cant add

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
+var app = new express ();
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list(); // an array of object
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
-  console.log(tweets)
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm:true } );
+
 });
 
 router.get("/user/:name", function (req ,res){
@@ -21,5 +23,26 @@ router.get("/tweets/:id", function (req ,res){
   var list = tweetBank.find({id:id})
   res.render ('index',{ title: 'Tweet ID #' + id, tweets: list } )
     })
+
+// parse application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({ extended: false }))
+
+//parse application/json
+app.use(bodyParser.json())
+
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   //res.end(JSON.stringify(req.body, null, 2))
+// })
+
+
+router.post('/tweets', function(req, res) {
+  console.log(req.body)
+  // var name = req.body.name;
+  // var text = req.body.text;
+  // tweetBank.add(name, text);
+  // res.redirect('/');
+});
 
 module.exports = router;
